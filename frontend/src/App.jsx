19e7b620e0772d9ac8ec0c771400974894fc3209
jakaws <jakaws_93@hotmail.com>
2026-04-20 11:22:25 +0200

    add styling to profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Testing from './components/Testing'
 import './App.css'
+import JakawProfil from './components/JakawProfil'
 
 function App() {
 
   return (
     <div> 
-      <h1>TEST</h1> 
+      <JakawProfil/>
       <Testing />
     </div>
    
---
+++ frontend/src/components/JakawProfil.jsx
@@ -0,0 +1,12 @@
+
+
+export default function JakawProfil(){
+    return(
+        <article className="jakaw-profil">
+            <img src="/images/jakaw.png" alt="Profilbilde av Jakaw" />
+            <h2>Jakaw Salehzada</h2>
+            <a href="mailto:jakaws@hiof.no">jakaws@hiof.no</a>
+            <p>Informasjonteknologi</p>
+        </article>
+    )
+}
