efc4f12f1db7e5526178d289c950bc7dbb8c9f6c
FAF <fridvinf@hiof.no>
2026-04-20 10:30:40 +0200

    Skapt mitt komponent
    Lagt til komponent med min info og bilde

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Testing from './components/Testing'
 import './App.css'
+import AlexComponent from './components/AlexComponent'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div> 
       <h1>TEST</h1> 
       <Testing />
+      <AlexComponent />
     </div>
    
   )
---
+++ frontend/src/components/AlexComponent.jsx
@@ -0,0 +1,15 @@
+
+export default function(){
+
+
+    return(
+        <article>
+            <h2>Fridvin Alex Fridriksson</h2>
+            <img src="alexbilde.gif" alt="Bilde av Alex" />
+            <a href="mailto:fridvinf@hiof.no">Send meg E-post!</a>
+            <p>Bachelorstudie: <a href="https://www.hiof.no/studier/programmer/itbinf-bachelorstudium-i-informasjonssystemer/oppbygging/">Informasjonssytemer</a></p>
+
+        </article>
+
+    )
+}
