430bd7c342c283532f06ae963de256f7efea4f38
Sim-kort <simen.a.l1106@gmail.com>
2026-04-20 10:16:51 +0200

    ok

---
+++ frontend/src/components/SimenProfil.jsx
@@ -0,0 +1,12 @@
+export default function SimenProfil() {
+    return (
+        <article>
+        <img src="https://picsum.photos/id/15/200/300" alt="Simen-Profil" />
+        <h2>
+                Simen
+        </h2>
+        <a href="mailto:simenala@hiof.no">Epost</a>
+        <p>Informasjonsteknologi </p>
+        </article>
+    );
+}
