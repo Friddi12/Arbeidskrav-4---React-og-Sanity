b618a8dfe92a390db84e25b4e178331f5f3a5389
Sim-kort <simen.a.l1106@gmail.com>
2026-04-22 13:17:37 +0200

    oppdatert css

--- frontend/src/components/SimenProfil.jsx
+++ frontend/src/components/SimenProfil.jsx
@@ -6,7 +6,7 @@ export default function SimenProfil() {
         <h2>
                 Simen
         </h2>
-        <a href="mailto:simenala@hiof.no">Epost</a>
+        <a href="mailto:simenala@hiof.no">Send epost til meg!</a>
         <a href="https://www.hiof.no/studier/programmer/itekaa-informasjonsteknologi-arsstudium/index.html?gad_source=1&gad_campaignid=23345746750&gbraid=0AAAAAD7gzsek0m_V6BAS9uVEvUvbnWeUn&gclid=CjwKCAjwnZfPBhAGEiwAzg-VzDEr1H4ur_iY-uGwTM3cXl-Zbs5vgZICoOhw41KQRYM5cnqiXPh56xoCTRMQAvD_BwE">Informasjonsteknologi, Årstudium</a>
         </article>
     );
