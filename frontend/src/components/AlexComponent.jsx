deb7665facaa2a6a2df08588b065e95ef9d319d4
FAF <fridvinf@hiof.no>
2026-04-22 12:16:48 +0200

    Bilde fix

--- frontend/src/components/AlexComponent.jsx
+++ frontend/src/components/AlexComponent.jsx
@@ -5,7 +5,7 @@ export default function(){
     return(
         <article id="alex-component">
             <h2>Fridvin Alex Fridriksson</h2>
-            <img src="alexbilde.gif" alt="Bilde av Alex" />
+            <img src="/alexbilde.gif" alt="Bilde av Alex" />
             <a href="mailto:fridvinf@hiof.no">Send meg E-post!</a>
             <p>Bachelorstudie: <a href="https://www.hiof.no/studier/programmer/itbinf-bachelorstudium-i-informasjonssystemer/oppbygging/">Informasjonssytemer</a></p>
 
