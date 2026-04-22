62fe0c07f30973427442cc90d61a37c40319651f
Sim-kort <simen.a.l1106@gmail.com>
2026-04-22 13:29:39 +0200

    oppdatert css til simen og jsx fil med navn og tekst på epost

--- frontend/src/components/SimenProfil.jsx
+++ frontend/src/components/SimenProfil.jsx
@@ -2,10 +2,8 @@ export default function SimenProfil() {
     return (
         <article id='simen-profil'>
         <img src="/Mitt-Bilde-2.jpeg" alt="Simen-Profil" />
-        <h2>
-                Simen
-        </h2>
-        <a href="mailto:simenala@hiof.no">Epost</a>
+        <h2>Simen Alexander Larsen</h2>
+        <a href="mailto:simenala@hiof.no">Send epost til meg!</a>
         <a href="https://www.hiof.no/studier/programmer/itekaa-informasjonsteknologi-arsstudium/index.html?gad_source=1&gad_campaignid=23345746750&gbraid=0AAAAAD7gzsek0m_V6BAS9uVEvUvbnWeUn&gclid=CjwKCAjwnZfPBhAGEiwAzg-VzDEr1H4ur_iY-uGwTM3cXl-Zbs5vgZICoOhw41KQRYM5cnqiXPh56xoCTRMQAvD_BwE">Informasjonsteknologi, Årstudium</a>
         </article>
     );
